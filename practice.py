3c291112171b7a558f079e1375f49094f71c7e5d
snedube <sinethemba.dube99@gmail.com>
2024-01-24 14:50:17 +0200

    My First Commit

---
+++ practice.py
@@ -0,0 +1,10 @@
+height = input("What is your height?")
+if float(height) > 150:
+    age = input("What is your age?")
+    if int(age) >=  16:
+        print("You can join the school")
+    else:
+        print("Youre too young my frinf")
+else:
+    print("Cant Join the school")
+    
